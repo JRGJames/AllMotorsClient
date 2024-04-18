7bb0c290bc7598a1e7d18220cd887e9cce9ed7ea
Jaume Rosello Gomez <jarogos14@gmail.com>
2024-04-18 20:03:41 +0200

    Progress on the carousel

--- src/app/components/shared/routed/home/home.component.ts
+++ src/app/components/shared/routed/home/home.component.ts
@@ -6,14 +6,15 @@ import { API_URL } from 'src/environment/environment';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 
 export class HomeComponent implements OnInit, OnDestroy {
   url = API_URL;
   popularCars: ICar[] = [];
-  currentPage: number = 0;
+  currentPage: number = 1;
   autoChangePage: any;
+  direction: number = 0;
 
   constructor(
     private carService: CarService,
@@ -22,7 +23,35 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.loadPopularCars();
     this.startAutoChangePage();
+    this.eventListener();
   }
+
+  eventListener() {
+    const slider = document.getElementById('slider');
+    const buttons = document.querySelectorAll('.buttons');
+
+    if (slider) { 
+      slider.addEventListener('transitionend', () => {
+        if (this.direction === -1) {
+          slider.appendChild(slider.firstElementChild as HTMLElement);
+        } else if (this.direction === 1) {
+          slider.prepend(slider.lastElementChild as HTMLElement);
+        }
+
+        slider.style.transition = 'none';
+        slider.style.transform = 'translateX(0)';
+        buttons.forEach((button) => {
+
+        });
+
+        setTimeout(() => {
+          slider.style.transition = 'all 0.5s';
+        });
+      });
+    }
+  }
+
+
 
   ngOnDestroy() {
     if (this.autoChangePage) {
@@ -50,20 +79,51 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   prevPage() {
+    const slider = document.getElementById('slider');
+    const carousel = document.getElementById('carousel');
+    
+    if (slider) {
+      if (this.direction === -1) {
+        slider.appendChild(slider.firstElementChild as HTMLElement);
+        this.direction = 1;
+      }
+      this.direction = 1;
+      slider.style.transform = 'translateX(20%)';
+      slider.style.transition = 'all 0.5s';
+      
+    }
+    if (carousel) {
+      carousel.style.justifyContent = 'flex-end';
+    }
+
     if (this.currentPage > 1) {
       this.currentPage--;
     } else {
       this.currentPage = this.popularCars.length;
     }
+
     this.resetAutoChangePage();
   }
 
   nextPage() {
+    const slider = document.getElementById('slider');
+    const carousel = document.getElementById('carousel');
+    this.direction = -1;
+
+    if (slider) {
+      slider.style.transform = 'translateX(-20%)';
+      slider.style.transition = 'all 0.5s';
+    }
+    if (carousel) {
+      carousel.style.justifyContent = 'flex-start';
+    }
+
     if (this.currentPage < this.popularCars.length) {
       this.currentPage++;
     } else {
       this.currentPage = 1;
     }
+
     this.resetAutoChangePage();
   }
 
